9f5db0af827e49795707bd256c4eb01c3da520ff
henrigevenois <henri.gevenois@proton.me>
2026-05-29 10:18:37 +0200

    [ADD] actuator endpoints

--- src/main/java/be/aidji/flexieureka/FlexiEurekaApplication.java
+++ src/main/java/be/aidji/flexieureka/FlexiEurekaApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class FlexiEurekaApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(FlexiEurekaApplication.class, args);
     }
 
